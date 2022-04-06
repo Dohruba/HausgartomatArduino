a5b94b64699921504b622f6ab8e63001540ccb46
Dohruba <ricardohendrichs@gmail.com>
2022-04-06 23:03:14 +0200

    Updating documentation

--- arduinoToUnity003/arduinoToUnity003.ino
+++ arduinoToUnity003/arduinoToUnity003.ino
@@ -1,3 +1,16 @@
+/*
+ * This code is meant to provide a Unity application with the values
+ * of different sensors and turn on and off different equipments that
+ * correspond to each value. The arduino only sends this information
+ * or modifies the state of an equipment, upon a request from the
+ * application. When asked, it measures the requested value 
+ * and provides feedback to the application or modifies the state
+ * of the requested equipment. 
+ * 
+ * It´s message is composed of three parts: 
+ * [Type of sensor read][Value read][State of the corresponding equipment]
+ */
+
 #include <SoftwareSerial.h>
 #include <SerialCommand.h>
 #include <OneWire.h>
@@ -11,21 +24,19 @@ OneWire oneWire(TEMP);
 DallasTemperature sensors(&oneWire);
 DeviceAddress insideThermometer;
 
-//Light sensor variables ///////////
+//Light sensor variables //////////////////
 int light = 0;
 bool ledIsOn = false;
 
-//Temperature sensor variables////////////
+//Temperature sensor variables ////////////
 int tempCs = 0;
 bool fanIsOn = false;
 
-//Moisture Sensor variables////////
+//Moisture Sensor variables ///////////////
 //A0 Value1: Air;
 //A0 Value2: Water;
 const int wet = 359;
 const int dry = 763;
-
-//Water Pump variables/////////////
 int percentageHumidity = 0;
 bool pumpIsOn = false;
 
@@ -65,9 +76,13 @@ void loop() {
       sCmd.readSerial();
   delay(200);
 }
+
 ////////////////////////////////////////////////////////////////////////////////////////////////
-////////// Light Management //////////////////////////////////////////////////////////////////
+////////// Light Management ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * Update the value read by the photoresistor.
+ */
 void lightCheck(const char *command){
   void lightCheck();
 }
@@ -82,6 +97,9 @@ void sendLightToUnity(){
   writeToUnity(ledIsOn, light, "l");
 }
 
+/*
+ * Turn on the lamp
+ */
 void ledOn (const char *command){
   ledOn();
 }
@@ -89,6 +107,9 @@ void ledOn (){
   ledIsOn = true;
   switchPin(LED,ledIsOn);
 }
+/*
+ * Turn off the lamp
+ */
 void ledOff (const char *command){
   ledOff();
 }
@@ -97,18 +118,8 @@ void ledOff (){
     switchPin(LED,ledIsOn);
 }
 
-/**
- * Voltage to Lux (Lumen/m^2) L = BR^m 
- * L: Light intensity
- * B:
- * R: Resistance
- * m: 
- */
-void voltageToLux (){
-  
-}
 ////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////// Temp Management ////////////////////////
+//////////////////////////////////////////////////// Temp Management ///////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////
 void tempCheck(const char *command){
   tempCheck();
@@ -125,7 +136,9 @@ void sendTempToUnity(){
   writeToUnity(fanIsOn, tempCs, "t");
 }
 
-
+/*
+ * Turn on the fan
+ */
 void fanOn (const char *command){
   fanOn();
 }
@@ -133,6 +146,9 @@ void fanOn (){
   fanIsOn=true;
   switchPin(FAN,fanIsOn);
 }
+/*
+ * Turn off the fan
+ */
 void fanOff (const char *command){
   fanOff();
 }
@@ -143,45 +159,43 @@ void fanOff (){
 ////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////// Temp Measurement ////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////
-
+//Look for source and put it here :D
 void startTempSensor(){
   sensors.begin();
   Serial.print(sensors.getDeviceCount(), DEC);
   if (sensors.isParasitePowerMode()) Serial.println("ON");
   if (!sensors.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0"); 
-  printAddress(insideThermometer);
+  //printAddress(insideThermometer);
   sensors.setResolution(insideThermometer, 9);
 }
 
-// function to print the temperature for a device
+/*
+ * function to print the temperature for a device
+ */
 float printTemperature(DeviceAddress deviceAddress)
 {
   float tempC = sensors.getTempC(deviceAddress);
   if(tempC == DEVICE_DISCONNECTED_C) 
   {
-    //Serial.println("Error: Could not read temperature data");
     return;
   }
-  //Serial.print("Temp C: ");
-  //Serial.println(tempC);
-
   return tempC;
 }
 
-void printAddress(DeviceAddress deviceAddress)
+/*void printAddress(DeviceAddress deviceAddress)
 {
   for (uint8_t i = 0; i < 8; i++)
   {
     //if (deviceAddress[i] < 16) Serial.print("0");
     //Serial.print(deviceAddress[i], HEX);
   }
-}
+}*/
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////// Capacitive Moisture Sensor /////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /**
- * Check earth humidity.
+ * Update the humidity value read by the Capacitive Moisture Sensor
  */
 void humidityCheck(const char *command){
   humidityCheck();
@@ -200,6 +214,9 @@ void sendHumidityToUnity(){
   writeToUnity(pumpIsOn, percentageHumidity, "h");
 }
 
+/*
+ * Turn on the pump
+ */
 void pumpOn (const char *command){
   pumpOn();
 }
@@ -209,6 +226,9 @@ void pumpOn (){
     switchPin(PUMP,pumpIsOn);
   }
 }
+/*
+ * Turn off the pump
+ */
 void pumpOff (const char *command){
   pumpOff();
 }
@@ -224,7 +244,7 @@ void pumpOff (){
 /////////////////////////////////////// General Functions /////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /**
- * Function to turn an equipment on or off, depending on it´s desired state.
+ * Function to turn a specific equipment on or off, depending on it´s desired state.
  */
 void switchPin(int pinNumber, bool equipmentState){
   if(equipmentState){
@@ -234,7 +254,10 @@ void switchPin(int pinNumber, bool equipmentState){
   }
 }
 /**
- * 
+ * Method to write a message to the Serial Port, which will be read in the
+ * unity application.
+ * The message is composed of three parts: 
+ * [State of the corresponding equipment][Value read][Type of sensor read]
  */
 void writeToUnity(bool equipmentON, int sensorValue, String type){
   String actualState = "";
@@ -251,9 +274,3 @@ void writeToUnity(bool equipmentON, int sensorValue, String type){
   Serial.flush();
   delay(50);
 }
-
-void checkSensorStatus(){
-  //Must implement
-}
-
-//Have to update the write to Unity method, so that it works with the new schema
